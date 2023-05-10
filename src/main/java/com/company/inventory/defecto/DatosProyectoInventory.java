5bbdcd95a89619204cf4f99df1c7897092bfb804
alcaudon71 <alcaudon71@gmail.com>
2023-05-10 16:36:56 +0200

    Primer commit del proyecto

---
+++ src/main/java/com/company/inventory/defecto/DatosProyectoInventory.java
@@ -0,0 +1,18 @@
+package com.company.inventory.defecto;
+
+public class DatosProyectoInventory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * 
+		 *	PROYECTO: 	SpringBoot y Angular. Creando aplicaciones como Fullstack
+		 *	AUTOR:		Alfredo Leal
+		 *	URL:		https://ust-global.udemy.com/course/spring-boot-y-angular-creando-aplicaciones-como-fullstack/learn/lecture/30918400#overview
+		 * 
+		 */
+		
+	}
+
+}
