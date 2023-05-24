550c506d2f7e80999c7b42029e337780f6e68057
alcaudon71 <alcaudon71@gmail.com>
2023-05-24 10:15:14 +0200

    Buscar categoria por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,34 @@ public class CategoryRestController {
 	@Autowired
 	private ICategoryService service;
 	
+	/**
+	 * Controlador para invocar al servicio de obtencion de todas las categorias
+	 * @return	ResponseEntity	Datos de todas las categorias existentes
+	 */
 	// endpoint: /api/v1/categories
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories() {
 		
 		ResponseEntity<CategoryResponseRest> response = service.search();
+		
+		return response;
+		
+	}
+	
+	/**
+	 * Controlador para invocar al servicio de obtencion de categorias por Id
+	 * @param  id   			ID de la categoria
+	 * @return ResponseEntity	Datos de la categoria buscada
+	 */
+	// endpoint: /api/v1/categories/{Id}
+	// @PathVariable ---> recoge el endpoint de entrada en formato "Decoded"
+	//                    http://localhost:8080/api/v1/categories/abc
+	// @RequestParam ---> recoge el endpoint de entrada en formato "Encoded"
+	//  				  http://localhost:8080/api/v1/categories?id=abc
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
+		
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		
 		return response;
 		
--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -41,6 +43,48 @@ public class CategoryServiceImpl implements ICategoryService {
 		} catch (Exception e) {
 			
 			response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+			e.getStackTrace();
+			
+			ResponseEntity<CategoryResponseRest> respError = new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			return respError;
+			
+		}
+		
+		ResponseEntity<CategoryResponseRest> respuesta = new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+		
+		return respuesta;
+		
+	}
+
+	/**
+	 * Busqueda de una Categoria por Id
+	 */
+	@Transactional(readOnly=true)
+	@Override
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		// TODO Auto-generated method stub
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			
+			Optional<Category> category = categoryDao.findById(id);
+			
+			if (category.isPresent()) {
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				// Rellenar el metadata de la respuesta
+				response.setMetadata("Respuesta ok", "00", "Categoria encontrada");
+			} else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no encontradda");
+				ResponseEntity<CategoryResponseRest> respError = new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+				return respError;
+			}
+			
+		} catch (Exception e) {
+			
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar por Id");
 			e.getStackTrace();
 			
 			ResponseEntity<CategoryResponseRest> respError = new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
